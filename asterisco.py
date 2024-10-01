95288cf587af8b294aec182dfd39ab2420abd295
xander-18 <xanderdinho1@gmail.com>
2024-10-01 11:41:21 -0500

    nuevos files

---
+++ asterisco.py
@@ -0,0 +1,5 @@
+for i in range(1, 10):
+    print("*" * i, i)
+
+for i in range(8, 0, -1):
+    print("*" * i, i)
--- exercise.py
+++ exercise.py
@@ -4,7 +4,7 @@ respuesta = True
 for n in list:
     if n in repeticion:
         repeticion[n] += 1
-        if repeticion[n] == 3:                                                                                                                                            
+        if repeticion[n] == 3:
             respuesta = False
     else:
         repeticion[n] = 1
